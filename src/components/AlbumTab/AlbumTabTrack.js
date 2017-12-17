7d8724c393e1d072ac84767048d803af3eb88a38
wjsc <walterscarano@outlook.com>
2017-12-17 08:03:38 -0300

    SearchTab fix

--- src/components/AlbumTab/AlbumTabTrack.js
+++ src/components/AlbumTab/AlbumTabTrack.js
@@ -4,12 +4,9 @@ class AlbumTabTrack extends React.Component {
 	render(){
 		return (
 			<div className="track">
-				<i className="play_track fa fa-play-circle">
-				</i>
-				<i className="add_track fa fa-plus-circle">
-				</i>
-				<i className="fav_track fa fa-heart-o">
-				</i>
+				<i className="play_track fa fa-play-circle"></i>
+				<i className="add_track fa fa-plus-circle"></i>
+				<i className="fav_track fa fa-heart-o"></i>
 				<div className="track_number">
 					{this.props.track.no}
 				</div>
--- src/components/SearchTab/SearchTab.js
+++ src/components/SearchTab/SearchTab.js
@@ -24,13 +24,13 @@ class SearchTab extends React.Component {
 		}
 	}
 	renderArtists(){
-		return this.state.artists ? this.state.artists.map( artist =><SearchTabArtist key={artist} artist={artist}/>) : false;
+		return this.state.artists ? this.state.artists.map( artist =><SearchTabArtist key={artist._id} artist={artist}/>) : false;
 	}
 	renderAlbums(){
-		return this.state.albums ? this.state.albums.map( album =><SearchTabAlbum key={album} album={album}/>) : false;
+		return this.state.albums ? this.state.albums.map( album =><SearchTabAlbum key={album._id} album={album}/>) : false;
 	}
 	renderTracks(){
-		return this.state.tracks ? this.state.tracks.map( track =><SearchTabTrack key={track} track={track}/>) : false;
+		return this.state.tracks ? this.state.tracks.map( track =><SearchTabTrack key={track._id} track={track}/>) : false;
 	}
 	render() {
 		return (
@@ -46,13 +46,13 @@ class SearchTab extends React.Component {
 							</div>
 						</div>
 						<div className="albums">
-							<div className="title">{this.state.artists.length +' albums'}</div>
+							<div className="title">{this.state.albums.length +' albums'}</div>
 							<div className="results">
 								{this.renderAlbums()}
 							</div>
 						</div>
 						<div className="tracks">
-							<div className="title">{this.state.artists.length +' tracks'}</div>
+							<div className="title">{this.state.tracks.length +' tracks'}</div>
 							<div className="results">
 								{this.renderTracks()}
 							</div>
