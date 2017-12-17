85dbc8ac6657ee86572337f8dc45837130cd5ae6
wjsc <walterscarano@outlook.com>
2017-12-16 23:58:35 -0300

    Home in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ class Sidebar extends React.Component {
 	render() {
 		return (
 			<div className="sidebar">
+				<Link to="/"><SidebarElement id="sidebar-home" icon="fa fa-home" text="Home"/></Link>
 				<Link to="/search"><SidebarElement id="sidebar-search" icon="fa fa-search" text="Search"/></Link>
 				<Link to="/favorites"><SidebarElement id="sidebar-favorites" icon="fa fa-heart" text="Favorites"/></Link>
 				<Link to="/history"><SidebarElement id="sidebar-history" icon="fa fa-suitcase" text="History"/></Link>
