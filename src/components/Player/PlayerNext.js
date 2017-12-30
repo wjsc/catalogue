5b801a2fc57ec395e8bed7dc939f62166ee808ce
wjsc <walterscarano@outlook.com>
2017-12-30 08:47:56 -0300

    bugfix: remove action from next/prev

--- src/components/Player/PlayerNext.js
+++ src/components/Player/PlayerNext.js
@@ -3,7 +3,8 @@ import React from 'react';
 class PlayerNext extends React.Component {
 	render() {
 		return (
-				<div className={this.props.active ? 'player_next fa fa-step-forward' : 'player_next fa fa-step-forward inactive'} onClick={this.props.onclick}></div>
+				<div className={this.props.active ? 'player_next fa fa-step-forward' : 'player_next fa fa-step-forward inactive'} 
+					onClick={this.props.active && this.props.onclick}></div>
 			);
 	}
 }
--- src/components/Player/PlayerPrev.js
+++ src/components/Player/PlayerPrev.js
@@ -3,7 +3,8 @@ import React from 'react';
 class PlayerPrev extends React.Component {
 	render() {
 		return (
-				<div className={this.props.active ? 'player_prev fa fa-step-backward' : 'player_prev fa fa-step-backward inactive'} onClick={this.props.onclick}></div>
+				<div className={this.props.active ? 'player_prev fa fa-step-backward' : 'player_prev fa fa-step-backward inactive'} 
+					onClick={this.props.active && this.props.onclick}></div>
 			);
 	}
 }
