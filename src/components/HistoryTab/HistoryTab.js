c11c038ce2b372e49fcd79dcf20e92499cafe2df
wjsc <walterscarano@outlook.com>
2018-01-01 12:04:03 -0300

    Favorites in history Tab

--- src/components/HistoryTab/HistoryTab.js
+++ src/components/HistoryTab/HistoryTab.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './HistoryTab.css';
 import HistoryTabTrack from './HistoryTabTrack';
-import {fetchHistory, fetchTrack} from '../../calls.js';
+import {fetchHistory, fetchTrack, checkFavorites} from '../../calls.js';
 
 class HistoryTab extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			 tracks :[ ]
+			 tracks :[],
+			 favorites: []
 		};
 	}
 	componentDidMount(){
 		fetchHistory('ABCDEABCDEABCDEABCDEABCDEABCDEABCDEF')
 		.then( history => history.map(history => history.track ))
 		.then( tracks => tracks.join(','))
-		.then( tracks => fetchTrack(tracks))
-		.then( tracks => this.setState({tracks}));
+		.then( tracks => {
+			checkFavorites('ABCDEABCDEABCDEABCDEABCDEABCDEABCDEF', tracks)
+			.then( favorites => this.setState({favorites}));
+			fetchTrack(tracks)
+			.then( tracks => this.setState({tracks}));
+		})
+		
 	}
 	renderTracks(){
-		return this.state.tracks ? this.state.tracks.map((track)=><HistoryTabTrack key={track._id} track={track}/>) : false;
+		return this.state.tracks ? this.state.tracks.map((track)=><HistoryTabTrack key={track._id} track={track} favorite={this.state.favorites.find(f => f.track===track._id)}/>) : false;
 	}
 	render() {
 		return (
