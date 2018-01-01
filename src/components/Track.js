44b2cad585048397b76159c83342f586d823cb7a
wjsc <walterscarano@outlook.com>
2018-01-01 18:06:45 -0300

    Toggle Favorites finally working

--- src/components/Track.js
+++ src/components/Track.js
@@ -7,31 +7,35 @@ class Track extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			received: false,
-			favorite: this.props.favorite
+			favorite: null,
+			favoriteReceived: this.props.favorite
 		}
 		this.toggleFavorite = this.toggleFavorite.bind(this);
 		this.isCurrentTrack = this.isCurrentTrack.bind(this);
+		this.isFavorited = this.isFavorited.bind(this);
 	}
 	toggleFavorite(){
-		return this.state.favorite ? 
+		return this.isFavorited() ? 
 		removeFavorite('ABCDEABCDEABCDEABCDEABCDEABCDEABCDEF', this.props.track._id)
 		.then(res => this.setState( { favorite: false } ))
 		: insertFavorite('ABCDEABCDEABCDEABCDEABCDEABCDEABCDEF', this.props.track._id)
 		.then(res => this.setState( { favorite: true } ));
 	}
 	componentWillReceiveProps(nextProps){
-		!this.received && this.setState({received: true, favorite: nextProps.favorite});
+		nextProps.favorite !== this.state.favoriteReceived && this.setState({favoriteReceived: nextProps.favorite});
 	}
 	isCurrentTrack(){
 		return playerLink.getCurrentTrack() && playerLink.getCurrentTrack()._id===this.props.track._id;
+	}
+	isFavorited(){
+		return this.state.favorite !== null ? this.state.favorite : this.state.favoriteReceived;
 	}
 	render(){
 		return (
 			<div className={this.isCurrentTrack()?'current track':'track'} onDoubleClick={() => playerLink.playTrack(this.props.track)}>
 				<i className="play_track fa fa-play-circle" onClick={() => playerLink.playTrack(this.props.track)}></i>
 				<i className="add_track fa fa-plus-circle" onClick={() => playerLink.addTrack(this.props.track)}></i>
-				<i className={this.state.favorite ? "fav_track fa fa-heart" : "fav_track fa fa-heart-o"}
+				<i className={this.isFavorited() ? "fav_track fa fa-heart" : "fav_track fa fa-heart-o"}
 					onClick={this.toggleFavorite }></i>
 				<div className="track_number">
 					{this.props.track.no}
