0af30e2304f2fddea06617a60df2ec2d9e83272b
wjsc <walterscarano@outlook.com>
2018-03-22 11:48:01 -0300

    Bugsnag enabled

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import './common.css';
 import registerServiceWorker from './registerServiceWorker';
 import CatalogueApp from './components/CatalogueApp';
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
+
+const bugsnagClient = bugsnag('9982863f503b8ea8ad471f1473120748')
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 
 ReactDOM.render(
 	<HashRouter>
-		 <CatalogueApp/>
+		<ErrorBoundary>
+			<CatalogueApp/>
+		</ErrorBoundary>
 	</HashRouter>
   ,
   document.getElementById('root')
